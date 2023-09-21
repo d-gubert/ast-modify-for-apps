06b93e8c0761fa73bb6d185955729f65a02c1613
Douglas Gubert <douglas.gubert@gmail.com>
2023-09-21 11:58:50 -0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,7 @@
-// export { defaultTraveler } from 'npm:astravel';
-import * as astringMod from "https://esm.sh/astring@1.8.6";
-import { parse } from "https://esm.sh/acorn@8.10.0";
-import { fullAncestor } from "https://esm.sh/acorn-walk@8.2.0";
+import { generate } from "npm:astring@1.8.6";
+import { parse } from "npm:acorn@8.10.0";
+import { fullAncestor } from "npm:acorn-walk@8.2.0";
 
-export const astring = astringMod;
+export const astring = { generate };
 export const acorn = { parse };
 export const acornWalk = { fullAncestor };
